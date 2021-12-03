6facd07fdfc63c7991fec5803c0e15475d4e0d09
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-12-03 13:57:02 +0800

    [ParkingLot][Nicole] -feat: initial project

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,40 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ParkingLotTest {
+
+    // Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket
+
+    @Test
+    void should_return_ticket_when_park_car_given_parking_lot_and_car_() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //when
+        Ticket ticket = parkingLot.park(car);
+
+        //then
+        assertNotNull(ticket);
+    }
+
+    // Case 2 - Given a parking lot without any position, and a car, When park the car, Then return nothing
+
+    @Test
+    void should_return_null_when_park_car_given_full_parking_lot_and_car_() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car car = new Car();
+        parkingLot.park(car); // full parkingLot
+
+        //when
+        Ticket ticket = parkingLot.park(car);
+
+        //then
+        assertNull(ticket);
+    }
+}
