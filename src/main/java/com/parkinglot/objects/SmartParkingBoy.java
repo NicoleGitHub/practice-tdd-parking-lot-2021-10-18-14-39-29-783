5f7aa519cd3663828a443a25fbfc105892ce536d
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-12-03 18:07:51 +0800

    [ParkingLot][Nicole] -feat: should_parked_car_in_first_parking_lot_when_park_car_given_smart_parking_boy_with_two_equal_size_parking_lot

---
+++ src/main/java/com/parkinglot/objects/SmartParkingBoy.java
@@ -0,0 +1,16 @@
+package com.parkinglot.objects;
+
+import java.util.ArrayList;
+
+public class SmartParkingBoy {
+
+    private ArrayList<ParkingLot> parkingLots;
+
+    public SmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public Ticket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -0,0 +1,28 @@
+package com.parkinglot;
+
+import com.parkinglot.objects.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SmartParkingBoyTest {
+    @Test
+    void should_parked_car_in_first_parking_lot_when_park_car_given_smart_parking_boy_with_two_equal_size_parking_lot() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ArrayList<>(List.of(parkingLot1, parkingLot2)));
+
+        //when
+        Ticket ticket = smartParkingBoy.park(new Car());
+
+        //then
+        assertNotNull(ticket);
+        assertEquals(9, parkingLot1.getAvailablePosition());
+        assertEquals(10, parkingLot2.getAvailablePosition());
+    }
+}
