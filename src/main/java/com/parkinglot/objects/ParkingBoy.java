c53ba03f802524b572d11c9c36599c45ca97c91e
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-12-03 16:39:38 +0800

    [ParkingLot][Nicole] -feat: should_return_ticket_when_park_car_given_standard_parking_boy_manage_one_parking_lot_and_carand passed

---
+++ src/main/java/com/parkinglot/objects/ParkingBoy.java
@@ -0,0 +1,13 @@
+package com.parkinglot.objects;
+
+public class ParkingBoy {
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public Ticket park(Car car) {
+        return parkingLot.park(car);
+    }
+}
---
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -0,0 +1,25 @@
+package com.parkinglot;
+
+import com.parkinglot.objects.Car;
+import com.parkinglot.objects.ParkingBoy;
+import com.parkinglot.objects.ParkingLot;
+import com.parkinglot.objects.Ticket;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingBoyTest {
+    @Test
+    void should_return_ticket_when_park_car_given_standard_parking_boy_manage_one_parking_lot_and_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        //when
+        Ticket ticket = parkingBoy.park(new Car());
+
+        //then
+        assertNotNull(ticket);
+    }
+
+}
