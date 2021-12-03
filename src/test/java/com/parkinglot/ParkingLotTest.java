7bc3962ea70496f348b2640f288032bd5bf312d2
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-12-03 14:26:42 +0800

    [ParkingLot][Nicole] -feat: case 4

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -42,12 +42,30 @@ public class ParkingLotTest {
         //given
         ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
-        parkingLot.park(car); // full parkingLot
+        parkingLot.park(car);
 
         //when
         Car fetchedCar = parkingLot.fetch(car.getTicket());
 
         //then
         assertEquals(car, fetchedCar);
+    }
+
+    @Test
+    void should_return_right_car_with_right_ticket_when_fetch_twice_given_two_parked_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        parkingLot.park(car1);
+        parkingLot.park(car2);
+
+        //when
+        Car fetchedCar1 = parkingLot.fetch(car1.getTicket());
+        Car fetchedCar2 = parkingLot.fetch(car2.getTicket());
+
+        //then
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car1, fetchedCar2);
     }
 }
