33d0a781cfca1460d6efe93efc1c166e8d6f6893
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-12-03 18:01:36 +0800

    [ParkingLot][Nicole] -feat: should_no_person_available_when_park_car_given_standard_parking_boy_manage_two_parking_lots_both_unavailable_and_full passed

--- src/main/java/com/parkinglot/objects/ParkingBoy.java
+++ src/main/java/com/parkinglot/objects/ParkingBoy.java
@@ -10,8 +10,7 @@ public class ParkingBoy {
     }
 
     public Ticket park(Car car) {
-//        return parkingLots.stream().reduce((parkingLot, nextParkingLot) -> parkingLot.getAvailablePosition() > 0 ? parkingLot : nextParkingLot).get().park(car);
-        return parkingLots.stream().filter(parkingLot -> parkingLot.getAvailablePosition() > 0).findFirst().get().park(car);
+        return parkingLots.stream().reduce((parkingLot, nextParkingLot) -> parkingLot.getAvailablePosition() > 0 ? parkingLot : nextParkingLot).get().park(car);
     }
 
     public Car fetch(Ticket ticket) {
